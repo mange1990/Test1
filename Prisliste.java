d326baa0386b32027717ce5d15c3cd9d37b6144a
Magnus <renen90@hotmail.com>
2020-01-16 15:39:37 +0100

    first commit

---
+++ Prisliste.java
@@ -0,0 +1,2 @@
+No_;Description;Manufacturer item no;Manufacturer;Min sales;Brand;Product Group Code;Unit Price - DKK;Inventory;Retail price DKK;EUR;ProductLevel1;ProductLevel2;ProductLevel3;ProductLevel4;EAN;ImageUrl;ETA;Order QTY
+000121;"Idehæfte Idéer til hørlærred";;;1;HOBBY2YOU;4121;5,24;111;0;0,70;Bøger;Brochure;;;;http://www.vestergaardgroup.com/catalog/images/000121-big.jpg;;
